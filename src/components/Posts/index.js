e04517ea1aa47beca8fd1d0f5cfac8077e089d16
Hernan Casé <hcase@batan.coop>
2020-01-24 09:58:58 -0300

    Added Post Form

--- src/components/Posts/index.js
+++ src/components/Posts/index.js
@@ -1,17 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './styles.css'
 
 function Posts(props){
+
+    const initialState = {
+        title: '',
+        body: ''
+      };
+
+    const [ Form, setForm ] = useState(initialState);
+
+    const handleInputChange = e => {
+        setForm({...Form, [e.target.name]: e.target.value})
+      };
+    
+    const handleSubmit = e => {
+        e.preventDefault();
+        if ( Form.title && Form.body ){
+            console.log(Form)
+        } else {
+            console.log('Falta completar un campo.')
+        }
+    };
+
     return(
         <div className="posts">
-            <div className="title">Posts</div>
-            <div className="form">
-                <input placeholder='Title' type="text" className="post-title" />
-                <button className="post-display">Display Posts</button>
-                <input placeholder='Body' type="text-area" className="post-text"/>
-                <button className="post-add">Add Post</button>
-                <button className="post-reset">Reset</button>
-            </div>
+            <div className="title">Posts Example</div>
+            <form className="form" onSubmit={ handleSubmit }>
+                <input placeholder='Title' type="text" name="title" className="post-title" onChange={ handleInputChange }/>
+                <input placeholder='Body' type="text-area" name="body" className="post-text" onChange={ handleInputChange }/>
+                <div className='add-reset-buttons'> 
+                <button className="post-add btn btn-success mr-1" type="submit">Add Post</button>
+                <button className="post-reset btn btn-secondary" type='reset' onClick={ () => setForm(initialState) }>Reset</button>
+                </div>
+                <button className="post-display btn btn-primary">Display Posts</button>
+            </form>
         </div>
     );
 }
