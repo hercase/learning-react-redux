61d9a5e1346727d3d7e605e040430c84172c5acc
Hernan Casé <hcase@batan.coop>
2020-01-24 07:59:27 -0300

    Starting with Posts Examples

--- src/components/Posts/index.js
+++ src/components/Posts/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import './styles.css'
 
 function Posts(props){
     return(
         <div className="posts">
             <div className="title">Posts</div>
             <div className="form">
-                <input type="text" />
-                <button>Display Posts</button>
-                <input type="text-area" />
-                <button>Add Posts</button>
-                <button>Reset</button>
+                <input placeholder='Title' type="text" className="post-title" />
+                <button className="post-display">Display Posts</button>
+                <input placeholder='Body' type="text-area" className="post-text"/>
+                <button className="post-add">Add Post</button>
+                <button className="post-reset">Reset</button>
             </div>
         </div>
     );
